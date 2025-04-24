88a10be71a122f2eea1cb8e0f80fe18edcfc3ab2
Lawrence Nevvon <root@lawrencelaptop>
2025-04-24 12:42:17 -0400

    Linter should now pass

--- src/reader/viewer.py
+++ src/reader/viewer.py
@@ -4,12 +4,12 @@
 
 
 def show(article: str) -> None:
-    """Show one article."""
-    print(article)
+	"""Show one article."""
+	# sometimes print(article)
 
 
-def show_list(site: str, titles: list[str]) -> None:
-    """Show list of articles."""
-    print(f"The latest tutorials from {site}")
-    for article_id, title in enumerate(titles):
-        print(f"{article_id:>3} {title}")
+# def show_list(site: str, titles: list[str]) -> None:
+# 	"""Show list of articles."""
+# 	# print(f"The latest tutorials from {site}") bla
+# 	for article_id, title in enumerate(titles):
+# 		print(f"{article_id:>3} {title}") bla
