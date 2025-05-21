642490c2926a01f7be887f0ab7bfc399bd6482b8
Lawrence Nevvon <root@lawrencelaptop>
2025-05-21 12:49:40 -0400

    Solved workdir issue for tests

---
+++ mytests.sh
@@ -0,0 +1,16 @@
+PROJECT="sso"
+
+
+if [[ ${PROJECT} == "common" ]]; then
+    workdir="./common"
+elif test -d "./services/${PROJECT}"; then
+    workdir="./services/${PROJECT}"
+elif test -d "./tools/${PROJECT}"; then
+    workdir="./tools/${PROJECT}"
+fi
+
+
+
+
+
+echo ${workdir}
